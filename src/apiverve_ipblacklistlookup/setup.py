7e69df32dc7cfa208c9a850cb6290bf8e5a849ed
APIVerve Team <hello@apiverve.com>
2025-03-28 17:27:16 -0500

    Bug Fixes

--- src/apiverve_ipblacklistlookup/setup.py
+++ src/apiverve_ipblacklistlookup/setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
 
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirements_file = os.path.join(lib_folder, 'requirements.txt')
+install_requires = ["requests >= 2.25.1", "setuptools >= 56.0.0"]
+if os.path.exists(requirements_file):
+    with open(requirements_file, 'r') as f:
+        install_requires = f.read().splitlines()
+
 setup(
     name='apiverve_ipblacklistlookup',
-    version='1.0.9',
+    version='1.1.9',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'requests',
-        'setuptools'
-    ],
-    description='IP Blacklist Lookup is a simple tool for looking up if an IP address is in a blacklist. It returns the blacklist name and the reason for the blacklist.',
+    install_requires=install_requires,
+    description='IP Blacklist Lookup is a simple tool for looking up if an IP address is in a blacklist. It returns if the IP is found in a blacklist.',
     author='APIVerve',
     author_email='hello@apiverve.com',
     url='https://apiverve.com',
